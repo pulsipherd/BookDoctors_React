9af2c796a7fed41a9b59cf199f38f7c0582a1d59
Deneb Pulsipher <pulsipherd@gmail.com>
2018-05-23 15:32:50 -0600

    code ability to add new book

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :body, :keywords)
+  end
